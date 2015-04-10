0edda55df21623dec68416722d67669510637b9b
voidbab <niko.halikka@gmail.com>
2015-04-10 12:46:10 +0300

    started work with loading images

--- MapEditor/Windows/NewTileSetDialog.xaml.cs
+++ MapEditor/Windows/NewTileSetDialog.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,14 +20,32 @@ namespace MapEditor.Windows
     /// </summary>
     public partial class NewTilesetDialog : Window
     {
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
         public NewTilesetDialog()
         {
             InitializeComponent();
         }
 
+        #region Event handlers
         private void loadTextureButton_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Images (*.png)|*.png|(*.jpeg)|*.jpeg|(*.jpg)|*.jpg";
+
+            // Loading the file failed, return.
+            if (!openFileDialog.ShowDialog().Value) return;
+
+            // Got file, load it.
+            tilesetImagePreview.Source = new BitmapImage(new Uri(openFileDialog.FileName));
 
         }
+        #endregion
     }
 }
