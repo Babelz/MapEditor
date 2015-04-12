780e9d3e5b8e5bd1c24c79ca4ca09cafed44fcad
Voidbab <niko.halikka@gmail.com>
2015-04-12 23:03:35 +0300

    small work woth tilelayer window, need to make bindings and models now

---
+++ MapEditorViewModels/LayersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MapEditorCore.Abstractions;
+using MapEditorCore.TileEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapEditorViewModels
+{
+    public sealed class LayersViewModel
+    {
+    }
+}
