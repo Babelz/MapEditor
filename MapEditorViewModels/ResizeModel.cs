99df23bc028628d90614bd199e27920d684a24b4
Voidbab <niko.halikka@gmail.com>
2015-04-14 13:48:23 +0300

    started to make resize dialog window

---
+++ MapEditorViewModels/ResizeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditorViewModels
+{
+    public sealed class ResizeModel
+    {
+        #region Fields
+        private readonly int maxWidth;
+        private readonly int maxHeight;
+
+        public int NewWidth;
+        public int NewHeight;
+        #endregion
+
+        public ResizeModel(int maxWidth, int maxHeight)
+        {
+            this.maxWidth = maxWidth;
+            this.maxHeight = maxHeight;
+        }
+
+        public bool WidthInBounds()
+        {
+            return NewWidth > 0 && NewWidth <= maxWidth;
+        }
+
+        public bool HeightInBounds()
+        {
+            return NewHeight > 0 && NewHeight <= maxHeight;
+        }
+
+        public bool InBounds()
+        {
+            return WidthInBounds() && HeightInBounds();
+        }
+    }
+}
